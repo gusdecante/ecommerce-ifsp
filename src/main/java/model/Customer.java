0e942731fffa269fcb9ed37827516afbb4e842c3
decante <gustavocamposantunes@gmail.com>
2020-08-15 16:05:11 -0300

    Addition of the class Customer With the acess methods and the attributes idCustomer and nameCustomer

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Customer {
+    private int idCustomer;
+    private String nameCustomer;
+    
+    public int getIdCustomer() {
+        return this.idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public String getNameCustomer() {
+        return this.nameCustomer;
+    }
+
+    public void setNameCustomer(String nameCustomer) {
+        this.nameCustomer = nameCustomer;
+    }
+}
---
+++ src/main/java/model/PessoaFisica.java
@@ -0,0 +1,10 @@
+package model;
+
+
+public class PessoaFisica extends Customer{
+    private String CPF;
+    private String RG;
+    private String date;
+
+    
+}
