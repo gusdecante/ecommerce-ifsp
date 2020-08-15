daf7d8b1afe25e5fec7ec00c2929a0f1eb353748
decante <gustavocamposantunes@gmail.com>
2020-08-15 16:09:38 -0300

    Addition of the class PessoaFísica That class extends Customer and have the attributes CPF, RG and dateOfBirth

--- src/main/java/model/PessoaFisica.java
+++ src/main/java/model/PessoaFisica.java
@@ -6,5 +6,29 @@ public class PessoaFisica extends Customer{
     private String RG;
     private String date;
 
+
+    public String getCPF() {
+        return this.CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return this.RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
     
 }
