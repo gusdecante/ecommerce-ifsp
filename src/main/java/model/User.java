2d4ec2863f357e40c86b7bdf543a71ece78f5ca0
decante <gustavocamposantunes@gmail.com>
2020-08-15 16:49:26 -0300

    Modification, addiction of the attribute token to the class User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,38 +5,47 @@ public class User {
     private String email;
     private String password;
     private int typeOfUser;//add token
+    private int token;
 
+    
     public int getIdUser() {
         return this.idUser;
     }
-
+    
     public void setIdUser(int idUser) {
         this.idUser = idUser;
     }
-
+    
     public String getEmail() {
         return this.email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getPassword() {
         return this.password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public int getTypeOfUser() {
         return this.typeOfUser;
     }
-
+    
     public void setTypeOfUser(int typeOfUser) {
         this.typeOfUser = typeOfUser;
     }
+    
+    public int getToken() {
+        return this.token;
+    }
 
-
+    public void setToken(int token) {
+        this.token = token;
+    }
+    
 }
