bea16b54b2521aa0690c474dc6db0e19ba610704
João Marcos <joaomarcossm2013@hotmail.com>
2020-08-17 12:44:37 -0300

    Class adress created

---
+++ src/main/java/model/Adress.java
@@ -0,0 +1,74 @@
+package model;
+
+public class Adress {
+    
+    private int idAdress,customerIdCustomer;
+    private String street,number,district,city,state,zipCode;
+
+
+    public int getIdAdress() {
+        return this.idAdress;
+    }
+
+    public void setIdAdress(int idAdress) {
+        this.idAdress = idAdress;
+    }
+
+    public int getCustomerIdCustomer() {
+        return this.customerIdCustomer;
+    }
+
+    public void setCustomerIdCustomer(int customerIdCustomer) {
+        this.customerIdCustomer = customerIdCustomer;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDistrict() {
+        return this.district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return this.zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+
+}
