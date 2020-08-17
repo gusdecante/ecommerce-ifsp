1963b1231f90a69dea52e6a1eeeb4f289a4c4bc5
João Marcos <joaomarcossm2013@hotmail.com>
2020-08-17 12:57:21 -0300

    Class paymentForm created

---
+++ src/main/java/model/PaymentForm.java
@@ -0,0 +1,25 @@
+package model;
+
+public class PaymentForm {
+    
+    private int idPaymentForm;
+    private String paymentForm;
+
+
+    public int getIdPaymentForm() {
+        return this.idPaymentForm;
+    }
+
+    public void setIdPaymentForm(int idPaymentForm) {
+        this.idPaymentForm = idPaymentForm;
+    }
+
+    public String getPaymentForm() {
+        return this.paymentForm;
+    }
+
+    public void setPaymentForm(String paymentForm) {
+        this.paymentForm = paymentForm;
+    }
+
+}
