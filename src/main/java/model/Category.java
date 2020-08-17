bb07b14d6c4da2c6c701b71d3e3b2ced2f24ec02
João Marcos <joaomarcossm2013@hotmail.com>
2020-08-17 12:40:05 -0300

    Model category created

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Category {
+    
+    private int idCategory;
+    private String category;
+
+
+    public int getIdCategory() {
+        return this.idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+
+}
