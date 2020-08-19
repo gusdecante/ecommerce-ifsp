6eb001d5030e8c4fb273049b14428fe45a37be4f
João Marcos <joaomarcossm2013@hotmail.com>
2020-08-19 13:15:28 -0300

    Class categoryDao is created and ModelDao is atualized

--- src/main/java/dao/ModelDao.java
+++ src/main/java/dao/ModelDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model./*class*/;
 
 import util.MysqlConection;
 
@@ -22,9 +23,10 @@ public class /*Class*/Dao {
     public boolean register/*class*/(/*class*/ i) {
         boolean isSuccess = false;
         try {
-            String query = "INSERT INTO /*class*/ (id_/*class*/, , , , ) VALUES (?, ?, ?, ?, ?);";
+            String query = "INSERT INTO /*table*/ (id_/*class*/, , , , ) VALUES (?, ?, ?, ?, ?);";
 
-            PreparedStatement st = con.prepareStatement(query); 
+            PreparedStatement st = con.prepareStatement(query); //Prepared the query
+
             st.setInt(1, 0);
             st.setString(2, i.get/*field*/());
             st.setString(3, i.get/*field*/());
@@ -33,6 +35,7 @@ public class /*Class*/Dao {
 
             st.execute(); //Execute the insert
             st.close(); //Close the Statment
+            con.close(); //Close the connection
             isSuccess = true;
             
         } catch (SQLException e) {
@@ -40,7 +43,6 @@ public class /*Class*/Dao {
             isSuccess = false;
         }
         
-        con.close(); //Close the connection
         return isSuccess;
     }
 
@@ -48,14 +50,14 @@ public class /*Class*/Dao {
     public List</*class*/> search/*class*/() throws SQLException, Exception {
 
         List</*class*/> lista = new ArrayList();
-        String query = "SELECT * FROM /*class*/ ;";
+        String query = "SELECT * FROM /*table*/ ;";
 
-        PreparedStatement st = con.prepareStatement(query);
+        PreparedStatement st = con.prepareStatement(query); //Prepared the query
         ResultSet rs = st.executeQuery(); //Execute the select
 
         while(rs.next()) {
-
             /*class*/ /*object*/ = new /*class*/();
+
             /*object*/.setId/*class*/(rs.getInt("id_/*class*/"));
             /*object*/.set/*field*/(rs.getString("/*field-db*/"));
             /*object*/.set/*field*/(rs.getString("/*field-db*/"));
@@ -63,10 +65,10 @@ public class /*Class*/Dao {
             /*object*/.set/*field*/(rs.getString("/*field-db*/"));
 
             lista.add(/*object*/);
-           
         }
-
-        con.close(); //Close the Statement
+        
+        st.close(); //Close the Statment
+        con.close(); //Close the connection
 
         return lista;
     }
@@ -75,21 +77,34 @@ public class /*Class*/Dao {
     public boolean update/*class*/(/*class*/ u) {
         boolean isSuccess = false;
         try {
-            String query = "UPDATE /*class*/ SET id_/*class*/ = ?, *field* = ?, , , ;";
-            PreparedStatement st = con.prepareStatement(query);
-            st.setInt(1, u.getId/*class*/());
+            String query = "UPDATE /*table*/ SET id_/*class*/ = ?, *field* = ?, , , ;";
+
+            PreparedStatement st = con.prepareStatement(query); //Prepared the query
+            //Select id informated 
+            List</*Class*/> l = new /*Class*/Dao().search/*Class*/(u.getId/*Class*/());
+                
+            for (/*Class*/ lc : l) {
+                st.setInt(1, lc.getId/*Class*/());
+            }
             st.setString(2, u.get/*field-db*/());
             st.setString(3, u.get/*field-db*/());
             st.setString(4, u.get/*field-db*/());
             st.setString(5, u.get/*field-db*/());
+
+            st.executeUpdate(); //Execute the update
+            st.close(); //Close the Statment
+            con.close(); //Close the connection
 
             isSuccess = true;
 
         } catch (SQLException e) {
             e.printStackTrace();
             isSuccess = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            isSuccess = false;
         }
-        con.close();
+
         return isSuccess;
     }
 
@@ -97,13 +112,15 @@ public class /*Class*/Dao {
     public boolean delete/*class*/(int d) {
         boolean isSuccess = false;
         try {
-            String query = "DELETE FROM /*class*/ WHERE id_/*class*/ = ?;";
+            String query = "DELETE FROM /*table*/ WHERE id_/*class*/ = ?;";
 
-            PreparedStatement st = con.prepareStatement(query); 
+            PreparedStatement st = con.prepareStatement(query); //Prepared the query
             st.setInt(1, d);
 
-            st.execute(); //Execute the insert
+            st.executeUpdate(); //Execute the Delete
             st.close(); //Close the Statment
+            con.close(); //Close the connection
+
             isSuccess = true;
             
         } catch (SQLException e) {
@@ -111,24 +128,23 @@ public class /*Class*/Dao {
             isSuccess = false;
         }
         
-        con.close(); //Close the connection
         return isSuccess;
     }
 
     //Select specifical register on the mysql table
-    public List</*class*/> search/*class*/(String parameter) throws SQLException, Exception {
+    public List</*class*/> search/*class*/(int id/*Class*/) throws SQLException, Exception {
 
         List</*class*/> lista = new ArrayList();
-        String query = "SELECT * FROM /*class*/ WHERE  = ?;";
+        String query = "SELECT * FROM /*table*/ WHERE id_/*Class*/ = ?;";
 
-        PreparedStatement st = con.prepareStatement(query);
-        st.setString(2, parameter);
+        PreparedStatement st = con.prepareStatement(query); //Prepared the query
+        st.setInt(2, id/*Class*/);
 
         ResultSet rs = st.executeQuery(); //Execute the select
 
         while(rs.next()) {
-
             /*class*/ /*object*/ = new /*class*/();
+
             /*object*/.setId/*class*/(rs.getInt("id_/*class*/"));
             /*object*/.set/*field*/(rs.getString("/*field-db*/"));
             /*object*/.set/*field*/(rs.getString("/*field-db*/"));
@@ -139,7 +155,8 @@ public class /*Class*/Dao {
            
         }
 
-        con.close(); //Close the Statement
+        st.close(); //Close the Statment
+        con.close(); //Close the connection
 
         return lista;
     }
