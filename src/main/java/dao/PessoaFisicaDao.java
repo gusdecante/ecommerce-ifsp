a2763fccbbbca396a8d460601676e40ad9e2b8ce
decante <gustavocamposantunes@gmail.com>
2020-08-19 13:18:04 -0300

    Change the insteance of PessoaFissica for p to pf, to make more undertandable

--- src/main/java/dao/PessoaFisicaDao.java
+++ src/main/java/dao/PessoaFisicaDao.java
@@ -16,38 +16,41 @@ public class PessoaFisicaDao {
     
     //Register normal customer, not an business
     //PessoaFísica extends User, so the attributer of user will be userd here as well
-    public void registerPessoaFisica(PessoaFisica p) throws SQLException {
+    public void registerPessoaFisica(PessoaFisica pf) throws SQLException {
         
         String query;
         //First - Insert User
         query = "INSERT INTO user (id_User, email, password, user_Tyoe, token) VALUES (?, ?, ?, ?, ?);";
         PreparedStatement st = con.prepareStatement(query);
-        st.setInt(1, p.getIdUser());
-        st.setString(2, p.getEmail());
-        st.setString(3, p.getPassword());
-        st.setInt(4, p.getTypeOfUser());
-        st.setString(5, p.getToken());
+        st.setInt(1, pf.getIdUser());
+        st.setString(2, pf.getEmail());
+        st.setString(3, pf.getPassword());
+        st.setInt(4, pf.getTypeOfUser());
+        st.setString(5, pf.getToken());
         st.execute();
 
         //Second - Insert Customer 
         query = "INSERT INT customer(id_Customer, user_id_User) VALUES(?, ?)";
         st = con.prepareStatement(query);
-        st.setInt(1, p.getIdCustomer());
-        st.setInt(2, p.getIdUser());
+        st.setInt(1, pf.getIdCustomer());
+        st.setInt(2, pf.getIdUser());
         st.execute();
 
         //Third - Insert Pessoa Física 
         query = "INSERT INTO pessoa_Fisica(id_Pessoa_Fisica, name_Customer, CPF, RG, date_Birth, customer_id_Customer) VALUES (?, ?, ?, ?, ?, ?)";
         st = con.prepareStatement(query);
-        st.setInt(1, p.getIdPessoaFisica());
-        st.setString(2, p.getNameCustomer());
-        st.setString(3, p.getCPF());;
-        st.setString(4, p.getRG());
-        st.setDate(5, p.getDateOfBirth());
-        st.setInt(6, p.getIdCustomer());
+        st.setInt(1, pf.getIdPessoaFisica());
+        st.setString(2, pf.getNameCustomer());
+        st.setString(3, pf.getCPF());;
+        st.setString(4, pf.getRG());
+        st.setDate(5, pf.getDateOfBirth());
+        st.setInt(6, pf.getIdCustomer());
         st.execute();
         st.close();
         con.close();
          
     }
+
+    //Listing PessoaFisica customer
+    
 }
