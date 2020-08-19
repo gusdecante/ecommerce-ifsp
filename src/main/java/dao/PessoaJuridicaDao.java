e2c42edba6041a8421a4274ff434f510ed605632
decante <gustavocamposantunes@gmail.com>
2020-08-18 23:06:32 -0300

    Addition of the contructor method for the class PessoaJuridicaDao

--- src/main/java/dao/PessoaJuridicaDao.java
+++ src/main/java/dao/PessoaJuridicaDao.java
@@ -1,5 +1,13 @@
 package dao;
 
+import java.sql.Connection;
+
+import util.MysqlConection;
+
 public class PessoaJuridicaDao {
-    
+    private Connection con;
+
+    public PessoaJuridicaDao() {
+        con = new MysqlConection().getConnection();
+    }
 }
