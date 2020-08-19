f8bea5e794fd05d353c2aeb5df73fa6cdc54f726
decante <gustavocamposantunes@gmail.com>
2020-08-18 23:18:40 -0300

    Addition of the attribute idPessoaJuridica and the access methods

--- src/main/java/model/PessoaJuridica.java
+++ src/main/java/model/PessoaJuridica.java
@@ -1,8 +1,17 @@
 package model;
 
 public class PessoaJuridica extends Customer{
+    private int idPessoaJuridica;
     private String cnpj;
     private String razaoSocial;
+
+    public int getIdPessoaJuridica() {
+        return this.idPessoaJuridica;
+    }
+
+    public void setIdPessoaJuridica(int idPessoaJuridica) {
+        this.idPessoaJuridica = idPessoaJuridica;
+    }
 
     public String getCnpj() {
         return this.cnpj;
