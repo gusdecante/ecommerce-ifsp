f4e31785d04ff774279616ebb83b9e8e40013797
decante <gustavocamposantunes@gmail.com>
2020-08-18 23:22:46 -0300

    Addition of the method registerPessoaJuridica into the Dao of PessoaJuridica

--- src/main/java/dao/PessoaJuridicaDao.java
+++ src/main/java/dao/PessoaJuridicaDao.java
@@ -1,7 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import model.PessoaJuridica;
 import util.MysqlConection;
 
 public class PessoaJuridicaDao {
@@ -9,5 +12,38 @@ public class PessoaJuridicaDao {
 
     public PessoaJuridicaDao() {
         con = new MysqlConection().getConnection();
+    }
+
+    //Register Businesses 
+    //PessoaJuridica extends customer who extends user, so all of the attributes will be here
+    public void registerPessoaJuridica(PessoaJuridica p) throws SQLException {
+        String query;
+        //First - Insert User
+        query = "INSERT INTO user (id_User, email, password, user_Tyoe, token) VALUES (?, ?, ?, ?, ?);";
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1, p.getIdUser());
+        st.setString(2, p.getEmail());
+        st.setString(3, p.getPassword());
+        st.setInt(4, p.getTypeOfUser());
+        st.setString(5, p.getToken());
+        st.execute();
+
+        //Second - Insert Customer 
+        query = "INSERT INT customer(id_Customer, user_id_User) VALUES(?, ?)";
+        st = con.prepareStatement(query);
+        st.setInt(1, p.getIdCustomer());
+        st.setInt(2, p.getIdUser());
+        st.execute();
+
+        //Third - Insert PessoaJuridica
+        query = "INSERT INT pessoa_Juridica(id_Pessoa_Juridica, CNPJ, razao_Social, customer_id_Customer) VALUES(?, ?, ?, ?)";
+        st = con.prepareStatement(query);
+        st.setInt(1, p.getIdPessoaJuridica());
+        st.setString(2, p.getCnpj());
+        st.setString(3, p.getRazaoSocial());
+        st.setInt(4, p.getIdCustomer());
+        st.execute();
+        st.close();
+        con.close();
     }
 }
