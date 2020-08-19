633e1cb46b0b4e0e0f995c6fb9e920773fcf0526
João Marcos <joaomarcossm2013@hotmail.com>
2020-08-18 22:50:02 -0300

    CRUD model for development other DAOs

--- src/main/java/dao/ModelDao.java
+++ src/main/java/dao/ModelDao.java
@@ -3,7 +3,9 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import util.MysqlConection;
 
@@ -12,63 +14,97 @@ public class ModelDao {
     private Connection con;
 
     //Mysql connection created
-    public Dao() {
+    public /*class*/Dao() {
         con = new MysqlConection().getConnection();
     }
 
     //Insert register on the mysql table
-    public void register( i) throws SQLException {
-        String query;
-        query = "INSERT INTO  (id_, , , , ) VALUES (?, ?, ?, ?, ?);";
+    public boolean register/*class*/(/*object*/ i) {
+        boolean isSuccess = false;
+        try {
+            String query = "INSERT INTO *TABLE* (id_, , , , ) VALUES (?, ?, ?, ?, ?);";
 
-        PreparedStatement st = con.prepareStatement(query); 
-        st.setInt(1, i.getId());
-        st.setString(2, i.get());
+            PreparedStatement st = con.prepareStatement(query); 
+            st.setInt(1, i.getId/*field*/());
+            st.setString(2, i.get/*field*/());
+            st.setString(3, i.get/*field*/());
+            st.setString(4, i.get/*field*/());
+            st.setString(5, i.get/*field*/());
 
-        st.execute(); //Execute the query
-        st.close(); //Close the Statment
+            st.execute(); //Execute the insert
+            st.close(); //Close the Statment
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            isSuccess = false;
+        }
+        
         con.close(); //Close the connection
+        return isSuccess;
     }
 
     //Select register on the mysql table
-    public List<> search() throws SQLException, Exception {
+    public List</*class*/> search/*class*/() throws SQLException, Exception {
 
-        List<> lista = new ArrayList();
-        String query = "SELECT * FROM  WHERE ;";
+        List</*class*/> lista = new ArrayList();
+        String query = "SELECT * FROM *TABLE* ;";
 
         PreparedStatement st = con.prepareStatement(query);
-        ResultSet rs = st.executeQuery();
+        ResultSet rs = st.executeQuery(); //Execute the select
 
         while(rs.next()) {
-             usr.setIdUser( rs.getInt("id_User"));
-             usr.setEmail(rs.getString("email"));
-             usr.setPassword(rs.getString("password"));
-             usr.setTypeOfUser(rs.getInt("type_User"));
-             usr.setToken(rs.getString("token"));
+
+            /*Class and object*/ = new /*class*/();
+            /*object*/.setId/*field*/(rs.getInt("id_/*field-db*/"));
+            /*object*/.set/*field*/(rs.getString("/*field-db*/"));
+
+            lista.add();
+           
         }
 
-        con.close();
-        return usr;
+        con.close(); //Close the Statement
+
+        return lista;
     }
 
-    /*This method changes the informations about the User, except for the type, 
-    this is only registered one time*/
-    public static boolean updateUser(User user) throws SQLException {
+    //Update register on the mysql table
+    public boolean update/*class*/(/*class*/ /*object*/) throws SQLException {
         boolean isSuccess = false;
         try {
-            String query = "UPDATE User SET id_User = ?, email = ?, password = ?, token = ? WHERE email = ?";
+            String query = "UPDATE *TABLE* SET id_ = ?, *field* = ?, , , ;";
             PreparedStatement st = con.prepareStatement(query);
-            st.setInt(1, user.getIdUser());
-            st.setString(2, user.getEmail());
-            st.setString(3, user.getPassword());
-            st.setString(4, user.getToken());//The type of user stay the same
+            st.setInt(1, /*object*/.getId/*field*/());
+            st.setString(2, /*object*/.get/*field-db*/());
 
             isSuccess = true;
-        } catch (Exception e) {
+
+        } catch (SQLException e) {
             e.printStackTrace();
             isSuccess = false;
         }
         con.close();
+        return isSuccess;
+    }
+
+    //delete register on the mysql table
+    public boolean delete/*class*/(/*object*/ d) throws SQLException {
+        boolean isSuccess = false;
+        try {
+            String query = "DELETE FROM *TABLE* WHERE *field* = ?;";
+
+            PreparedStatement st = con.prepareStatement(query); 
+            st.setInt(1, d.getId/*field*/());
+            st.setString(2, d.get/*field*/());
+
+            st.execute(); //Execute the insert
+            st.close(); //Close the Statment
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            isSuccess = false;
+        }
+        
+        con.close(); //Close the connection
         return isSuccess;
     }
     
