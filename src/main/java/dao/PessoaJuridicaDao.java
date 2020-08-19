c533651d34e8424f1391366e2d34bab6302a9bc3
decante <gustavocamposantunes@gmail.com>
2020-08-19 17:11:25 -0300

    Modification for the instance of PessoaJurifica from p to pj

--- src/main/java/dao/PessoaJuridicaDao.java
+++ src/main/java/dao/PessoaJuridicaDao.java
@@ -21,32 +21,32 @@ public class PessoaJuridicaDao {
 
     //Register Businesses 
     //PessoaJuridica extends customer who extends user, so all of the attributes will be here
-    public void registerPessoaJuridica(PessoaJuridica p) throws SQLException {
+    public void registerPessoaJuridica(PessoaJuridica pj) throws SQLException {
         String query;
         //First - Insert User
         query = "INSERT INTO user (id_User, email, password, user_Tyoe, token) VALUES (?, ?, ?, ?, ?);";
         PreparedStatement st = con.prepareStatement(query);
-        st.setInt(1, p.getIdUser());
-        st.setString(2, p.getEmail());
-        st.setString(3, p.getPassword());
-        st.setInt(4, p.getTypeOfUser());
-        st.setString(5, p.getToken());
+        st.setInt(1, pj.getIdUser());
+        st.setString(2, pj.getEmail());
+        st.setString(3, pj.getPassword());
+        st.setInt(4, pj.getTypeOfUser());
+        st.setString(5, pj.getToken());
         st.execute();
 
         //Second - Insert Customer 
         query = "INSERT INT customer(id_Customer, user_id_User) VALUES(?, ?)";
         st = con.prepareStatement(query);
-        st.setInt(1, p.getIdCustomer());
-        st.setInt(2, p.getIdUser());
+        st.setInt(1, pj.getIdCustomer());
+        st.setInt(2, pj.getIdUser());
         st.execute();
 
         //Third - Insert PessoaJuridica
         query = "INSERT INTO pessoa_Juridica(id_Pessoa_Juridica, CNPJ, razao_Social, customer_id_Customer) VALUES(?, ?, ?, ?)";
         st = con.prepareStatement(query);
-        st.setInt(1, p.getIdPessoaJuridica());
-        st.setString(2, p.getCnpj());
-        st.setString(3, p.getRazaoSocial());
-        st.setInt(4, p.getIdCustomer());
+        st.setInt(1, pj.getIdPessoaJuridica());
+        st.setString(2, pj.getCnpj());
+        st.setString(3, pj.getRazaoSocial());
+        st.setInt(4, pj.getIdCustomer());
         st.execute();
 
         //Fourth - Insert Address
