878df350b893d9d485e597f29a7e8ad00f5af41a
João Marcos <joaomarcossm2013@hotmail.com>
2020-09-05 19:16:41 -0300

    Model Controller atualized doGet revised

--- src/main/java/controller/modelController.java
+++ src/main/java/controller/modelController.java
@@ -1,185 +1,177 @@
-// package controller;
+package controller;
 
-// import java.io.IOException;
-// import java.io.PrintWriter;
-// import java.sql.SQLException;
-// import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
 
-// import javax.servlet.ServletException;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// import com.google.gson.Gson;
+import com.google.gson.Gson;
 
-// import dao./*Class*/Dao;
-// import model./*Class*/;
+import dao./*Class*/Dao;
+import model./*Class*/;
 
-// public class /*Class*/Controller extends HttpServlet{
+public class /*Class*/Controller extends HttpServlet{
 
-//     @Override
-//     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
-//         resp.setContentType("application/json");
-//         resp.setCharacterEncoding("UTF-8");
-//         req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
 
-//         PrintWriter saida = resp.getWriter();
+        PrintWriter saida = resp.getWriter();
 
-//         //CREATE JSON WITH QUERY FROM DB
-//         try {
-//             int parameter, n = 0;
+        //CREATE JSON WITH QUERY FROM DB
+        try {
+            int parameter, n = 0;
             
-//             if (req.getParameter("id") == null || req.getParameter("id").equals("")) {
-//                 parameter = 0;
-//             } else {
-//                 parameter = Integer.parseInt(req.getParameter("id"));
-//             }         
+            if (req.getParameter("id/*Class*/") == null || req.getParameter("id/*Class*/").equals("")) {
+                parameter = 0;
+            } else {
+                parameter = Integer.parseInt(req.getParameter("id/*Class*/"));
+            }         
 
-//             /*Class*/Dao d = new /*Class*/Dao();
-//             List</*Class*/> lst;
+            /*Class*/Dao d = new /*Class*/Dao();
+            List</*Class*/> lst;
 
-//             if (parameter == 0) {
-//                 lst = d.search/*Class*/();
-//             } else {
-//                 lst = d.search/*Class*/(parameter);
-//             }
+            if (parameter == 0) {
+                lst = d.search/*Class*/();
+            } else {
+                lst = d.search/*Class*/(parameter);
+            }
             
-//             n = lst.size();
+            n = lst.size();
 
-//             if (n == 0) {
-//                 saida.println("[ { \"result\" : \"Não há resultado\" } ]");
-//             } else {
-//                 saida.println("[");
+            if (n == 0) {
+                saida.println("[ { \"result\" : \"Não há resultado\" } ]");
+            } else {
+                saida.println("[");
 
-//                 for (/*Class*/ select/*Class*/ : lst) {
+                for (/*Class*/ select/*Class*/ : lst) {
+         
+                    String str/*Class*/ = new Gson().toJson(select/*Class*/);
 
-//                     select/*Class*/.get;
-//                     select/*Class*/.get;
-//                     select/*Class*/.get;
-//                     select/*Class*/.get;
-//                     select/*Class*/.get;
-//                     select/*Class*/.get;
-//                     select/*Class*/.get;
-                    
-//                     String str/*Class*/ = new Gson().toJson(select/*Class*/);
-
-//                     if(n == 1) {
-//                         saida.printf(str/*Class*/ + "]");
-//                     }else 
-//                         saida.println(str/*Class*/ + ",");
-//                     n--;
-//                 }
+                    if(n == 1) {
+                        saida.printf(str/*Class*/ + "]");
+                    }else 
+                        saida.println(str/*Class*/ + ",");
+                    n--;
+                }
             
-//             }
+            }
         
-//         } catch (SQLException e) {
-//             saida.println("[ { \"result\" : \"Erro S " + e.getMessage() + "\" } ]");
-//         } catch (NumberFormatException e) {
-//             e.printStackTrace();
-//             saida.println("[ { \"result\" : \"Erro N " + e.getMessage() + "\" } ]");
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//             saida.println("[ { \"result\" : \"Erro E " + e.getMessage() + "\" } ]");
-//         }
+        } catch (SQLException e) {
+            saida.println("[ { \"result\" : \"Erro S " + e.getMessage() + "\" } ]");
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            saida.println("[ { \"result\" : \"Erro N " + e.getMessage() + "\" } ]");
+        } catch (Exception e) {
+            e.printStackTrace();
+            saida.println("[ { \"result\" : \"Erro E " + e.getMessage() + "\" } ]");
+        }
 
-//         saida.flush();
-//         saida.close();
+        saida.flush();
+        saida.close();
 
 
-//     }
+    }
 
-//     @Override
-//     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
-//         resp.setContentType("application/json");
-//         resp.setCharacterEncoding("UTF-8");
-//         req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
 
-//         PrintWriter saida = resp.getWriter();
+        PrintWriter saida = resp.getWriter();
 
-//         //CREATE JSON WITH db RESULT OF TRANSATION
-//         try {
-//             // criar validação de usuário.
-//             //
-//             //
-//             if (
-//                 req.getParameter("") == null || req.getParameter("").equals("") ||
-//                 req.getParameter("") == null || req.getParameter("").equals("") ||
-//                 req.getParameter("") == null || req.getParameter("").equals("") ||
-//                 req.getParameter("") == null || req.getParameter("").equals("") ||
-//                 req.getParameter("") == null || req.getParameter("").equals("") ||
-//                 req.getParameter("") == null || req.getParameter("").equals("")
-//             ) {
-//                 saida.println("[ { \"result\" : \"Existem valores nulos\" } ]");
-//             } else {
-//                 /*Class*/ insert/*Class*/. = new /*Class*/();
+        //CREATE JSON WITH db RESULT OF TRANSATION
+        try {
+            // criar validação de usuário.
+            //
+            //
+            if (
+                req.getParameter("") == null || req.getParameter("").equals("") ||
+                req.getParameter("") == null || req.getParameter("").equals("") ||
+                req.getParameter("") == null || req.getParameter("").equals("") ||
+                req.getParameter("") == null || req.getParameter("").equals("") ||
+                req.getParameter("") == null || req.getParameter("").equals("") ||
+                req.getParameter("") == null || req.getParameter("").equals("")
+            ) {
+                saida.println("[ { \"result\" : \"Existem valores nulos\" } ]");
+            } else {
+                /*Class*/ insert/*Class*/. = new /*Class*/();
             
-//                 insert/*Class*/.setCategoryIdCategory(Integer.parseInt(req.getParameter("categoryIdCategory")));
-//                 insert/*Class*/.setColor(req.getParameter("color"));
-//                 insert/*Class*/.setFinishingProcess(req.getParameter("finishingProcess"));
-//                 insert/*Class*/.setCubaType(req.getParameter("cubaType"));
-//                 insert/*Class*/.setDescription(req.getParameter("description"));
-//                 insert/*Class*/.setStock(Integer.parseInt(req.getParameter("stock")));
-//                 insert/*Class*/.setUnitaryValue(Double.parseDouble(req.getParameter("unitaryValue")));
-//                 insert/*Class*/.setImageLink(req.getParameter("imageLink"));
+                insert/*Class*/.setCategoryIdCategory(Integer.parseInt(req.getParameter("categoryIdCategory")));
+                insert/*Class*/.setColor(req.getParameter("color"));
+                insert/*Class*/.setFinishingProcess(req.getParameter("finishingProcess"));
+                insert/*Class*/.setCubaType(req.getParameter("cubaType"));
+                insert/*Class*/.setDescription(req.getParameter("description"));
+                insert/*Class*/.setStock(Integer.parseInt(req.getParameter("stock")));
+                insert/*Class*/.setUnitaryValue(Double.parseDouble(req.getParameter("unitaryValue")));
+                insert/*Class*/.setImageLink(req.getParameter("imageLink"));
 
-//                 /*Class*/Dao pd = new /*Class*/Dao();
+                /*Class*/Dao pd = new /*Class*/Dao();
 
-//                 boolean ok = pd.register/*Class*/(insert/*Class*/);
+                boolean ok = pd.register/*Class*/(insert/*Class*/);
 
-//                 if(ok == 1)
-//                     saida.println("[ { \"result\" : \"Dados inseridos com sucesso\" } ]");
-//                 else
-//                     saida.println("[ { \"result\" : \"Falha na inserção de dados\" } ]");
-//             }                        
+                if(ok == 1)
+                    saida.println("[ { \"result\" : \"Dados inseridos com sucesso\" } ]");
+                else
+                    saida.println("[ { \"result\" : \"Falha na inserção de dados\" } ]");
+            }                        
 
-//         } catch (NumberFormatException e) {
-//             e.printStackTrace();
-//             saida.println("[ { \"result\" : \"Erro N " + e.getMessage() + "\" } ]");
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//             saida.println("[ { \"result\" : \"Erro E " + e.getMessage() + "\" } ]");
-//         }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            saida.println("[ { \"result\" : \"Erro N " + e.getMessage() + "\" } ]");
+        } catch (Exception e) {
+            e.printStackTrace();
+            saida.println("[ { \"result\" : \"Erro E " + e.getMessage() + "\" } ]");
+        }
 
-//     }
+    }
 
-//     @Override
-//     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        
-//         resp.setContentType("application/json");
-//         resp.setCharacterEncoding("UTF-8");
-//         req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
 
-//         PrintWriter saida = resp.getWriter();
+        PrintWriter saida = resp.getWriter();
 
-//         //CREATE JSON WITH QUERY FROM DB
-//         try {
-//             // criar validação de usuário.
-//             //
-//             //
+        //CREATE JSON WITH QUERY FROM DB
+        try {
+            // criar validação de usuário.
+            //
+            //
             
-//             if (req.getParameter("id") == null || req.getParameter("id").equals("")) {
-//                 saida.println("[ { \"result\" : \"Existem valores nulos\" } ]");
-//             } else {
+            if (req.getParameter("id") == null || req.getParameter("id").equals("")) {
+                saida.println("[ { \"result\" : \"Existem valores nulos\" } ]");
+            } else {
                 
-//                 /*Class*/Dao d = new /*Class*/Dao();
+                /*Class*/Dao d = new /*Class*/Dao();
 
-//                 int ok = d.delete/*Class*/(Integer.parseInt(req.getParameter("id")));
+                int ok = d.delete/*Class*/(Integer.parseInt(req.getParameter("id")));
 
-//                 if(ok == 1)
-//                     saida.println("[ { \"result\" : \"Dado excluido com sucesso\" } ]");
-//                 else
-//                     saida.println("[ { \"result\" : \"Falha na exclusão\" } ]");
-//             }
+                if(ok == 1)
+                    saida.println("[ { \"result\" : \"Dado excluido com sucesso\" } ]");
+                else
+                    saida.println("[ { \"result\" : \"Falha na exclusão\" } ]");
+            }
             
-//         } catch (NumberFormatException e) {
-//             e.printStackTrace();
-//             saida.println("[ { \"result\" : \"Erro N " + e.getMessage() + "\" } ]");
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//             saida.println("[ { \"result\" : \"Erro E " + e.getMessage() + "\" } ]");
-//         }
-//     }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            saida.println("[ { \"result\" : \"Erro N " + e.getMessage() + "\" } ]");
+        } catch (Exception e) {
+            e.printStackTrace();
+            saida.println("[ { \"result\" : \"Erro E " + e.getMessage() + "\" } ]");
+        }
+    }
 
-// }
+}
